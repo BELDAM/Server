32abeb90636748da10f1f31025909592ec2aa74b
D34D10CK <leonard.berney@epfl.ch>
2015-06-03 11:14:08 +0200

    make code compile

--- src/characters/Elf/Mage.java
+++ src/characters/Elf/Mage.java
@@ -1,7 +1,34 @@
 package characters.Elf;
 
+import Interface.FightInterface;
+
 /**
  * Created by bastiangardel on 25.05.15.
  */
 public class Mage extends ElfDecorator {
+
+    @Override
+    public int getSpeed() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setSpeed(int speed) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void attack(FightInterface character) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void takeDmg(int dmg) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void die() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
